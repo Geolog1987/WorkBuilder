c7228d4e3c3038f5f4395257812015256fb43b8f
Aleksey Kodanev <akodanev@mail.ru>
2024-04-15 10:35:07 +0300

    first commit

---
+++ Person.java
@@ -0,0 +1,87 @@
+package Builder;
+import java.util.Objects;
+import java.util.OptionalInt;
+
+public class Person {
+    protected final String name;
+    protected final String surname;
+    int age;
+    String address;
+    public Person(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+    public Person(String name, String surname, int age) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+    }
+    public boolean hasAge() {
+        if(name != null){
+            return true;
+        }
+        return false;
+    }
+    public boolean hasAddress() {
+        if(address != null){
+            return true;
+        }
+        return false;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public String getSurname() {
+        return surname;
+    }
+    public OptionalInt getAge() {
+        return OptionalInt.of(age);
+    }
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        if(hasAddress()){
+            this.address = address;
+        }
+    }
+    public void happyBirthday() {
+        if(hasAge()){
+            age++;
+        }
+    }
+    public PersonBuilder newChildBuilder() {
+        return new PersonBuilder().setSurname(surname).setAddress(address);
+    }
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", age=" + age +
+                ", address='" + address + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return age == person.age && name.equals(person.name) && surname.equals(person.surname) && Objects.equals(address, person.address);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, surname);
+    }
+}
+
+
+
+
+
+
+
---
+++ PersonBuilder.java
@@ -0,0 +1,44 @@
+package Builder;
+
+public class PersonBuilder {
+    String name;
+    String surname;
+    int age;
+    String address;
+
+    public PersonBuilder setName(String name) {
+       if(name == null || name.isEmpty()){
+           throw new IllegalArgumentException("Не корректный ввод поля имя");
+       } this.name = name;
+       return this;
+    }
+
+    public PersonBuilder setSurname(String surname) {
+        if(surname == null || surname.isEmpty()){
+            throw new IllegalArgumentException("Не корректный ввод поля фамилия");
+        } this.surname = surname;
+        return this;
+    }
+
+    public PersonBuilder setAge(int age) {
+        if(age < 0 || age > 120){
+            throw new IllegalArgumentException("Не корректный ввод поля возраст");
+        } this.age = age;
+        return this;
+    }
+
+    public PersonBuilder setAddress(String address) {
+        this.address = address;
+        return this;
+    }
+    public Person build() throws IllegalStateException {
+        Person person;
+        if (name == null || surname == null)
+            throw new IllegalStateException("Не корректный ввод полей имя и фамилия");
+        if (age < 0) {
+            person = new Person(name, surname);
+        } else person = new Person(name, surname, age);
+        person.setAddress(address);
+        return person;
+    }
+}
